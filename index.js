5b1e15023fc9219fb97245bd23b5df1b92827b59
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-05 21:46:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 2*3.4//6.8
 5/2.5//2
 
-function add(a, b){
+function add(a,b){
   return a + b
 }
 
 function subtract(a,b){
   return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
 }
